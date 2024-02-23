1f581eeb5ff73080632980f134e380be3ad55b34
nicolahy <nicolas.molinaro@orange.fr>
2024-02-23 20:27:55 +0100

    go to 3.0 ko

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const glob = require('glob');
+
+const tsFiles = glob.sync(path.resolve(__dirname, "ts", "*.ts"));
+const entry = tsFiles.reduce((acc, filePath) => {
+    const fileName = path.basename(filePath, '.ts');
+    acc[fileName] = filePath;
+    return acc;
+}, {});
+
+module.exports = {
+    mode: "production",
+    entry: entry,
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
